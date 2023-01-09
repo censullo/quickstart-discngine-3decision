b1ce1cfcfb8a123116db120f4c58d833a92f3998
JonathanManass <jonathan.manassen@discngine.com>
2023-01-09 13:23:20 +0000

    added block to commit to final path in bucket

--- .semaphore/bin/rewrite_qss.sh
+++ .semaphore/bin/rewrite_qss.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sed -i 's/Default: aws-quickstart/Default: 3decision-eu-central-1/g' ~/quickstart-discngine-3decision/templates/*
-sed -i 's~quickstart-discngine-3decision/~quickstart-discngine-3decision-test/~g' ~/quickstart-discngine-3decision/templates/*
+sed -i 's~quickstart-discngine-3decision/~'${1}'~g' ~/quickstart-discngine-3decision/templates/*
